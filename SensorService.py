2e94a4971e05ce76c44905c96ab1a5b9064d4584
kimni <kim.nilsson87@gmail.com>
2017-03-02 20:52:23 +0100

    Made infinte loop, program has no abort function, to test if looping is an possability

--- SensorService.py
+++ SensorService.py
@@ -1,4 +1,5 @@
-from sense_hat import SenseHat
+from sense_emu import SenseHat
+import time
 
 sense = SenseHat()
 sense.show_message("Bootup")
@@ -6,10 +7,10 @@ temp = 255
 hum = 255
 pressure = 255
 
-temp = round(sense.get_temperature(), 1)
-hum = rount(sense.get_humidity(), 1)
-pressure = round(sense.get_pressure(), 1)
-
-msg = "Temp is {0}, Hum is {1}, Pressure ir {2}".format(temp, hum, pressure)
-
-sense.show_message(msg)
+while 1:
+    temp = round(sense.get_temperature(), 1)
+    hum = round(sense.get_humidity(), 1)
+    pressure = round(sense.get_pressure(), 1)
+    msg = "Temp is {0}, Hum is {1}, Pressure ir {2}".format(temp, hum, pressure)
+    sense.show_message(msg)
+    time.sleep(5)
